9c0532cd40ca9d6fea25492db80e92960c854e73
Anton Österblad <antos600@student.liu.se>
2015-10-20 12:28:45 +0200

    Structure for Light class

---
+++ src/Light.cpp
@@ -0,0 +1,14 @@
+#include "Light.h"
+
+Light::Light() {
+	 color = glm::vec3(1.0f, 1.0f, 0.0f);
+   position = glm::vec3(1.0, 0.0, 0.0);
+}
+
+Light::Light(glm::vec3 c, glm::vec3 p) {
+	color = c;
+	position = p;
+	// intesity?
+}
+
+Light::~Light() {}
